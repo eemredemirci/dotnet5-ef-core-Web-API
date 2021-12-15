95a7f63f4b9996ba3178e6305d36daab09e0547c
Emre Demirci <eemredemirci@gmail.com>
2021-12-15 18:18:54 +0300

    Add files via upload

---
+++ Emre_Demirci_Odev_1/Emre_Demirci_Odev_1/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using Emre_Demirci_Odev_1.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emre_Demirci_Odev_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class CategoryController : ControllerBase
+    {
+        NORTHWNDContext context = new NORTHWNDContext();
+        // Get Cagetory Info All
+        public IActionResult GetCategory()
+        {
+
+            List<Category> categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet("{categoryId}")]
+        public IActionResult GetCategoryByID(int categoryId)
+        {
+            List<Category> categories = context.Categories.Where(categories => categories.CategoryId == categoryId).ToList();
+            if (categories.Count != 0)
+            {
+                return Ok(categories);
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return CreatedAtAction("GetCategoryByID", "Category", new { categoryId = category.CategoryId }, category);
+        }
+
+        [HttpGet("AllProductByID/{Id}")]
+        public IActionResult AllProductByID(int Id)
+        {
+            List<Product> products = context.Products.Where(product => product.CategoryId == Id).ToList();
+            if (products.Count != 0)
+            {
+                return Ok(products);
+            }
+            return NoContent();
+
+        }
+
+    }
+}
---
+++ Emre_Demirci_Odev_1/Emre_Demirci_Odev_1/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Emre_Demirci_Odev_1.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emre_Demirci_Odev_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        NORTHWNDContext context = new NORTHWNDContext();
+
+        public IActionResult GetProduct()
+        {
+
+            List<Product> products = context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{productId}")]
+        public IActionResult GetProductByID(int productId)
+        {
+            List<Product> products = context.Products.Where(product => product.ProductId == productId).ToList();
+            if (products.Count != 0)
+            {
+                return Ok(products);
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return CreatedAtAction("GetProductByID", "Product", new { productId = product.ProductId }, product);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return NoContent();
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            Product updated = context.Products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            updated.ProductName = product.ProductName;
+            updated.QuantityPerUnit = product.QuantityPerUnit;
+            context.SaveChanges();
+            return CreatedAtAction("GetProductByID", "Product", new { productId = product.ProductId }, product);
+        }
+
+
+
+    }
+}
